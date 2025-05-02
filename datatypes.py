882e1149bcc609be8581a8a7a7ba1e03ccc10936
Maaz Ahmed Hussain <s.maazahmedhussain@gmail.com>
2025-05-02 15:00:22 +0530

    python basics

--- datatypes.py
+++ datatypes.py
@@ -1,6 +1,11 @@
-num1= int(input("enter a number:"))
-num2= int(input("enter a number: "))
-if(num1>num2):
-    print("num1 is greater",num1)
+# num1= int(input("enter a number:"))
+# num2= int(input("enter a number: "))
+# if(num1>num2):
+#     print("num1 is greater",num1)
+# else:
+#     print("num2 is greater",num2)
+
+if True:
+    print("hello world ")
 else:
-    print("num2 is greater",num2)
+    print("bye")
