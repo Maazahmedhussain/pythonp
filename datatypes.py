28d02cd475e6968585553b1dc4d88ce51d411034
Maaz Ahmed Hussain <s.maazahmedhussain@gmail.com>
2025-05-02 15:09:27 +0530

    for loop

--- datatypes.py
+++ datatypes.py
@@ -5,7 +5,25 @@
 # else:
 #     print("num2 is greater",num2)
 
-if True:
-    print("hello world ")
-else:
-    print("bye")
+# if True:
+#     print("hello world ")
+# else:
+#     print("bye")
+    
+    
+    
+    
+#range(1,11,1) : it isa  function which takes a argument 
+# start 
+# stop
+# increment 
+# print(range(0,11,1))
+
+# loops: It isa mechanism that reepates the task untll the condition is true
+
+for i in range(1,11,1):
+    print(i)
+    
+    
+    
+    
