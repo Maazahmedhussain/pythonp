841af89d73b113a8ce29dcfafab92a996c95a986
Maaz Ahmed Hussain <s.maazahmedhussain@gmail.com>
2025-05-05 14:44:09 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,31 @@
+# num= int(input("enter the number  of table: "))
+# for i in range(1,11,1):
+#     print(num, "x ",i,"=",num*i)
+    
+    # neated loops   outer lopp nd inner lopp
+    
+# for i in range(2,11,1):
+#     for j in range(1,11,1):
+#         print(i,"x",j,"=",i*j)
+#     print()
+
+
+# num=int(input("plz enter a number to check "))
+# flag=True
+# for i in range(2,num,1):
+#     if num%i==0:
+#         flag=False
+#         break
+    
+#     if flag==True:
+#         print("it is a prime number")   
+#     else:
+#         print(" it is not a prime ")
+        
+        
+x=1
+while False:
+    print(x)
+    x+=1
+print("hello")
+    
