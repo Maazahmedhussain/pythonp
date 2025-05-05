593900d3dfc992c920057ecabb80a4d726a54859
Maaz Ahmed Hussain <s.maazahmedhussain@gmail.com>
2025-05-05 14:54:24 +0530

    functions

--- functions.py
+++ functions.py
@@ -1,4 +1,14 @@
+# def greeting(x,y=11):
+#     for i in range(1,y,1):
+#         print("hi",x)
+# greeting("maaz",10)
+
+
 def greeting(x,y):
-    for i in range(1,6,1):
-        print("hi",x,y)
-greeting("maaz","ahmed")
+    return x*y
+a=greeting(5,5)
+print(a)
+if a>10:
+    print("hello")
+else:
+    print("bye")
