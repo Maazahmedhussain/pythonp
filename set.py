bf1a65066bf4573d37cf12a8e94ba59ed3ce73a1
Maaz Ahmed Hussain <s.maazahmedhussain@gmail.com>
2025-05-06 14:51:37 +0530

    sets

---
+++ set.py
@@ -0,0 +1,13 @@
+myset={10,20,"maaz",True,10}
+set={10,20,"hi",False,30}
+myset.add("hello")
+print(myset)
+print(myset.union(set))
+# all elemants 
+# print(myset)
+# for i in myset:
+#     print(i)
+
+
+
+print(myset.intersection(set))
