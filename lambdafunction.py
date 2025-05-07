c0d352350359a8f10b8882146856c987140971bd
Maaz Ahmed Hussain <s.maazahmedhussain@gmail.com>
2025-05-07 14:33:09 +0530

    lambda

--- lambdafunction.py
+++ lambdafunction.py
@@ -1,8 +1,15 @@
-def test(x):
-    print(x)
-# test(6)
-# it can perform miin operation not complex
-# it is siomple and withpiut name function . It is a keyword .it always reeturns the value . 
+# def test(x):
+#     print(x)
+# # test(6)
+# # it can perform miin operation not complex
+# # it is siomple and withpiut name function . It is a keyword .it always reeturns the value . 
 
-x= lambda a,b :a*b
-print(x(6,5))
+
+# x= lambda a,b :a*b
+# print(x(6,5))
+
+
+list=[2,4,3,5,7,8,8,9,11,23,14]
+z= filter(lambda x:x%2==0,list)
+for i in z :
+    print(i ," is  a evan no    ")
