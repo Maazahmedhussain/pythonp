3b7a222183a6a5da2c30ad59030f69c2db2e6182
Maaz Ahmed Hussain <s.maazahmedhussain@gmail.com>
2025-05-07 14:19:08 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+dict={
+    "name": "maaz ahmed",
+    "date_of_joining": "1-june-2025",
+    "salary": 40000
+}
+# print(dict["salary"])
+for i,j in dict.items():
+    print(i,j)
--- set.py
+++ set.py
@@ -1,13 +1,15 @@
 myset={10,20,"maaz",True,10}
 set={10,20,"hi",False,30}
-myset.add("hello")
-print(myset)
-print(myset.union(set))
-# all elemants 
+# myset.add("hello")
 # print(myset)
+# myset.remove("hello")
+myset.update(set)
+# print(myset.union(set))
+# all elemants 
+print(myset)
 # for i in myset:
 #     print(i)
 
 
 
-print(myset.intersection(set))
+# print(myset.intersection(set))
